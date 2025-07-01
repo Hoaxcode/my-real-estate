a1e7c14edb191c63356102a236d76ca24c432729
Mostafa Ragab <Hoaxcode@yahoo.com>
2025-07-02 02:12:12 +0300

    connecting to MongoDB with mongoose

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,24 @@ const url = process.env.URL;
 const morgan = require("morgan");
 if (process.env.NODE_ENV === "DEVELOPMENT") app.use(morgan("dev"));
 
+//* connecting to MongoDB with mongoose
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.DB_LOCAL)
+  .then(() => {
+    console.log("Connected to database successfully");
+  })
+  .catch((error) => {
+    console.log(`Database connection error:\n{error}`);
+    process.exit(1);
+  });
+
 //* routing
 app.get("/", (req, res) => {
   res.send("Hello Node-Express");
 });
 
+//* Strating express server
 app.listen(port, () =>
   console.log(`Server starting on port :${port} in ${node_env} mode.\n${url}`)
 );
