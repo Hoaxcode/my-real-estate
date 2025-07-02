0da2dd4620d9254cde7a4c65f2858bc5b2a4ba50
Mostafa Ragab <Hoaxcode@yahoo.com>
2025-07-02 21:43:41 +0300

    update the sign up API route and encrypt the password using bcryptjs

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,17 @@
 const UserModel = require("../models/userModel");
+const bcrypt = require("bcryptjs");
 
 //=> POST : Creates a new user
 const signup = async (req, res) => {
   const { username, email, password } = req.body;
-  const newUser = new UserModel({ username, email, password });
-  await newUser.save();
-  res.status(201).json("User created successfully");
+  const hashedPassword = bcrypt.hashSync(password, 10); // to hash a password
+  const newUser = new UserModel({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully");
+  } catch (error) {
+    res.status(500).json(error.message); // show the error message defiend in user schema in userModel
+  }
 };
 
 module.exports = signup;
